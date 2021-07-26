c65289535098dec10a15d1af02869668fc3523bd
jafigueroa-dev <jafigueroa692@gmail.com>
2021-07-26 13:13:20 -0500

    Add project files.

---
+++ TaskManagementApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+    using System;
+
+namespace TaskManagementApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TaskManagementApp/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManagementApp.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string TaskDescription { get; set; }
+        public string Status { get; set; } = "Incomplete";
+
+        public Task()
+        {
+
+        }
+    }
+}
